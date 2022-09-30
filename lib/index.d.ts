4e07aa45670ccd60bc373a2703294f37ff05caaa
Ahmet SALIH <ahmet@binalyze.com>
2022-09-30 18:30:50 +0300

    Add template

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+/**
+ * Theme and logo for JupyterLab
+ */
+declare const plugin: JupyterFrontEndPlugin<void>;
+export default plugin;
